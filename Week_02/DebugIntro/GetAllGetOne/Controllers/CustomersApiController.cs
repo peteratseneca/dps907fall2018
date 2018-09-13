b3a2a14ca93c9a2a4bd736fc17470c1de8ba2e89
Peter McIntyre <peter.mcintyre@senecacollege.ca>
2018-09-13 09:00:10 -0400

    All HTTP methods, debugging, associated data

---
+++ Week_02/DebugIntro/GetAllGetOne/Controllers/CustomersApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+// Change the namespace to avoid class name collision
+namespace GetAllGetOne.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        // Reference to a manager object
+        private Manager m = new Manager();
+
+        // GET: api/CustomersApi
+        public IHttpActionResult Get()
+        {
+            // Attention 01 (web service) - Error - object reference not set to an instance of an object
+
+            // Just as it says... an object is null, and you're trying to use it
+            // Set a breakpoint near this line, then "step over" to get to the error
+            // Inspect the variable values as you step through the lines of code
+
+            // Create a new CustomerEditContactInfo object
+            // Intentionally leave out some property values
+            // To fix, simply un-comment out the Fax and Email lines of code below
+            var newCust = new CustomerEditContactInfo
+            {
+                //Fax = "(416) 661-4034",
+                //Email = "customer@example.com",
+                CustomerId = 2,
+                Phone = "(416) 491-5050"
+            };
+
+            // Now, attempt to use (call, reference) some values that are not set
+            // To fix, un-comment the Fax and Email lines of code above
+            var email = newCust.Email;
+            var emailLength = email.Length;
+
+            // End of error block; the remaining code will work as expected
+
+            // Fetch the collection
+            var c = m.CustomerGetAll();
+
+            // Pass the collection to the view
+            return Ok(c);
+        }
+
+        /*
+        // GET: api/CustomersApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/CustomersApi
+        public void Post([FromBody]string value)
+        {
+        }
+        */
+
+        // PUT: api/CustomersApi/5
+        public IHttpActionResult Put(int id, [FromBody]CustomerEditContactInfo editedItem)
+        {
+            // Ensure that an "editedItem" is in the entity body
+            if (editedItem == null)
+            {
+                return BadRequest("Must send an entity body with the request");
+            }
+
+            // Ensure that the id value in the URI matches the id value in the entity body
+            if (id != editedItem.CustomerId)
+            {
+                return BadRequest("Invalid data in the entity body");
+            }
+
+            // Ensure that we can use the incoming data
+            if (ModelState.IsValid)
+            {
+                // Attention 03 (web service) - Error - cause a validation error when saving to the store
+
+                // At this point in the method, newItem has passed the initial validation test
+                // Set a breakpoint near this line 
+                // So, let's invalidate some of its values (null and string too long),
+                // and pass it to the manager, which will attempt to save it to the data store
+                // Then, BOOM!, a DbEntity validation error will appear
+
+                // Set a required value to null
+                // To fix this, comment out the following line
+                editedItem.Email = null;
+
+                // Set another to a too-long value
+                // To fix this, comment out the following line
+                editedItem.Phone = "This string is too long. It exceeds the 24-character limit.";
+
+                // Attempt to do the update
+                var changedItem = m.CustomerEditContactInfo(editedItem);
+
+                if (changedItem == null)
+                {
+                    // Return HTTP 400
+                    return BadRequest("Cannot edit the object");
+                }
+                else
+                {
+                    // Return HTTP 200 with the changed item in the entity body
+                    return Ok<CustomerBase>(changedItem);
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        /*
+        // DELETE: api/CustomersApi/5
+        public void Delete(int id)
+        {
+        }
+        */
+    }
+}
---
+++ Week_02/DebugIntro/GetAllGetOne/Controllers/Manager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using GetAllGetOne.Models;
+
+namespace GetAllGetOne.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper components
+        MapperConfiguration config;
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add constructor code here
+
+            // Configure the AutoMapper components
+            config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+
+                // Attention 02 - Error - mapping error, missing AutoMapper map
+
+                // Un-comment the following line to fix
+
+                //cfg.CreateMap<Models.Customer, Controllers.CustomerBase>();
+                cfg.CreateMap<Controllers.CustomerBase, Controllers.CustomerEditContactInfoForm>();
+                cfg.CreateMap<Controllers.CustomerAdd, Models.Customer>();
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+        // Use AutoMapper to map objects, source to target
+        public IEnumerable<CustomerBase> CustomerGetAll()
+        {
+            return mapper.Map<IEnumerable<CustomerBase>>(ds.Customers);
+        }
+
+        public CustomerBase CustomerGetById(int id)
+        {
+            // Attempt to fetch the object
+            var o = ds.Customers.Find(id);
+
+            // Return the result, or null if not found
+            return (o == null) ? null : mapper.Map<CustomerBase>(o);
+        }
+
+        public CustomerBase CustomerAdd(CustomerAdd newItem)
+        {
+            // Attempt to add the new item
+            // Notice how we map the incoming data to the design model object
+            var addedItem = ds.Customers.Add(mapper.Map<Customer>(newItem));
+            ds.SaveChanges();
+
+            // If successful, return the added item, mapped to a view model object
+            return (addedItem == null) ? null : mapper.Map<CustomerBase>(addedItem);
+        }
+
+        public CustomerBase CustomerEditContactInfo(CustomerEditContactInfo newItem)
+        {
+            // Attempt to fetch the object
+            var o = ds.Customers.Find(newItem.CustomerId);
+
+            if (o == null)
+            {
+                // Problem - item was not found, so return
+                return null;
+            }
+            else
+            {
+                // Update the object with the incoming values
+                ds.Entry(o).CurrentValues.SetValues(newItem);
+                ds.SaveChanges();
+
+                // Prepare and return the object
+                return mapper.Map<CustomerBase>(o);
+            }
+        }
+
+        public bool CustomerDelete(int id)
+        {
+            // Attempt to fetch the object to be deleted
+            var itemToDelete = ds.Customers.Find(id);
+
+            if (itemToDelete == null)
+            {
+                return false;
+            }
+            else
+            {
+                // Remove the object
+                ds.Customers.Remove(itemToDelete);
+                ds.SaveChanges();
+
+                return true;
+            }
+        }
+    }
+}
